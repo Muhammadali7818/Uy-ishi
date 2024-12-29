c4af75ee16a4d06f5e67ccf1e305637c02c92139
Muhammadali7818 <ganibekovmuhammadali@gmail.com>
2024-12-29 21:41:03 +0500

    main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+class user1 :
+    Ism = "Farxod"
+    Familiya = "Tursunov"
+    Tugilgan_kuni = "2004"
+    yoshi = "20"
+
+class user2 :
+    Ism = "Aziz"
+    Familiya = "Hayitboyev"
+    Tugilgan_kuni = "2010"
+    yoshi = "14"
